443da0913ba4b898b83a63a582473c3c461859d5
Guilherme Albino <guilhermealbino33@gmail.com>
2023-06-27 10:30:13 -0300

    feat: add get paginated products

--- products/src/modules/products/repositories/IProductsRepository.ts
+++ products/src/modules/products/repositories/IProductsRepository.ts
@@ -1,6 +1,16 @@
 import { Product, ProductProps } from '../../../modules/shared/entities/Product';
+import Page from '../../shared/types/page';
+import { SortProducts } from '../enums/SortProducts';
 
+export interface IGetPaginatedProductsParams {
+  page?: number;
+  limit?: number;
+  name?: string;
+  code?: string;
+  sortBy?: SortProducts;
+}
 export interface IProductsRepository {
   findByCode(code: string): Promise<Product | null>;
+  getPaginatedProducts(params: IGetPaginatedProductsParams): Promise<Page<Product>>;
   create(product: ProductProps): Promise<void>;
 }
